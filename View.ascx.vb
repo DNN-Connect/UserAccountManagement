32b12dfa45fe0e57df5f145109da07a8e934402e
Robrecht Siera <github.rsiera@holoncom.eu>
2014-09-20 12:35:37 +0200

    fix: edit user buttons did not work if UrlRewriter was configured to use lowercase.

--- View.ascx.vb
+++ View.ascx.vb
@@ -2105,9 +2105,11 @@ Namespace Connect.Modules.UserManagement.AccountManagement
                 grdUsers.PageSize = Convert.ToInt32(drpPageSize.SelectedItem.Value)
             End If
 
-            grdUsers.Columns.FindByDataField("CreatedDate").Visible = (ctlRoles.SelectedNode.Value = PortalSettings.RegisteredRoleId.ToString)
-            grdUsers.Columns.FindByDataField("CreatedOnDate").Visible = (ctlRoles.SelectedNode.Value <> PortalSettings.RegisteredRoleId.ToString)
-            grdUsers.Columns.FindByDataField("Status").Visible = (ctlRoles.SelectedNode.Value <> PortalSettings.RegisteredRoleId.ToString)
+            If Not ctlRoles.SelectedNode Is Nothing Then
+                grdUsers.Columns.FindByDataField("CreatedDate").Visible = (ctlRoles.SelectedNode.Value = PortalSettings.RegisteredRoleId.ToString)
+                grdUsers.Columns.FindByDataField("CreatedOnDate").Visible = (ctlRoles.SelectedNode.Value <> PortalSettings.RegisteredRoleId.ToString)
+                grdUsers.Columns.FindByDataField("Status").Visible = (ctlRoles.SelectedNode.Value <> PortalSettings.RegisteredRoleId.ToString)
+            End If
 
         End Sub
 
